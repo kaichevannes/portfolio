51998021eba5432918985a3da04fc8d32d1bb3e9
Kai Chevannes <chevannes.kai@gmail.com>
2025-04-08 20:35:21 +0100

    build(ansible): change dnf install to ansible package directly

--- deploy/init.sh
+++ deploy/init.sh
@@ -2,12 +2,12 @@
 
 if command -v apt >/dev/null 2>&1; then
   sudo apt update && sudo apt install -y pipx
+  pipx ensurepath
+  pipx install --include-deps ansible
 elif command -v dnf >/dev/null 2>&1; then
-  sudo dnf update && sudo dnf install -y pipx
+  sudo dnf update && sudo dnf install -y ansible
 else
   echo "package manager not configured"
   exit 1
 fi
 
-pipx ensurepath
-pipx install --include-deps ansible
