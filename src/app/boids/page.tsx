c0a196795e84e652f3bd3ced5b9ad006bfc9d660
Kai Chevannes <chevannes.kai@gmail.com>
2025-05-23 16:32:32 +0100

    feat: Ensure link to algorithms course is on boids page

--- src/app/boids/page.tsx
+++ src/app/boids/page.tsx
@@ -2,6 +2,9 @@ import { styled } from '@linaria/react';
 
 export default function Boids() {
   return (
-    <div>TODO</div>
+    <div>
+      TODO
+      <a href='https://frontendmasters.com/courses/algorithms/'>Algorithms course</a>
+    </div>
   );
 }
