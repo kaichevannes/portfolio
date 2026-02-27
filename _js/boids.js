f8f0a09e18ad92bc8b90579f0a3586f003e23cce
Kai Chevannes <chevannes.kai@gmail.com>
2026-02-27 09:02:06 +0000

    feat: boids basic controls

--- _js/boids.js
+++ _js/boids.js
@@ -3,7 +3,7 @@ import init, * as wasm from "../_assets/wasm/wasm_boids.js";
 const wasmInstance = await init();
 await wasm.initThreadPool(navigator.hardwareConcurrency);
 wasm.init_panic_hook();
-const universe = wasm.Builder.from_preset(wasm.Preset.Basic)
+let universe = wasm.Builder.from_preset(wasm.Preset.Basic)
   .number_of_boids(500)
   .multithreaded(true)
   .number_of_boids_per_thread(200)
@@ -219,56 +219,205 @@ function initializeSlider({
   id,
   min,
   max,
-  defaultValue,
+  defaultValueCallback,
   logarithmic,
   numberOfDecimals = 0,
-  snapPoints,
+  snapPoints = [],
   func,
 }) {
   const input = document.getElementById(id);
   const output = document.querySelector(`output[for='${id}']`);
   const datalist = document.getElementById(`${id}-values`);
 
-  snapPoints.forEach((n) => {
-    const option = document.createElement("option");
-    option.value = logarithmic ? Math.log(n) / Math.log(max) : n;
-    option.label = n;
-    datalist.appendChild(option);
-  });
+  const actualToLog = (value) => {
+    const logMin = Math.log(min);
+    const logMax = Math.log(max);
+    return (Math.log(value) - logMin) / (logMax - logMin);
+  };
 
-  const unLog = (value, min, max) => {
+  const logToActual = (value) => {
     const logMin = Math.log(min);
     const logMax = Math.log(max);
     return Math.exp(logMin + value * (logMax - logMin));
   };
 
   input.addEventListener("input", () => {
-    const actual = logarithmic ? unLog(input.value, min, max) : input.value;
+    const actual = logarithmic ? logToActual(input.value) : Number(input.value);
     output.value = actual.toFixed(numberOfDecimals);
     func(actual);
   });
 
+  snapPoints.forEach((n) => {
+    const option = document.createElement("option");
+    option.value = logarithmic ? actualToLog(n) : n;
+    option.label = n;
+    datalist.appendChild(option);
+  });
+
   function resetSlider() {
-    input.value = Math.log(defaultValue) / Math.log(max);
-    output.value = unLog(
-      Math.log(defaultValue) / Math.log(max),
-      min,
-      max,
-    ).toFixed(numberOfDecimals);
+    input.value = logarithmic
+      ? actualToLog(defaultValueCallback())
+      : defaultValueCallback();
+    output.value = defaultValueCallback().toFixed(numberOfDecimals);
   }
 
   input.addEventListener("dblclick", resetSlider);
   resetSlider();
+  return resetSlider;
 }
 
-initializeSlider({
+let defaultNumberOfBoids = universe.get_number_of_boids();
+const resetNumberOfBoids = initializeSlider({
   id: "number-of-boids",
   min: 1,
   max: 4000,
-  defaultValue: 500,
+  defaultValueCallback: () => defaultNumberOfBoids,
   logarithmic: true,
   snapPoints: [1, 2, 3, 4, 5, 500],
-  func: universe.set_number_of_boids,
+  func: (x) => universe.set_number_of_boids(x),
+});
+
+let defaultDensity = universe.get_density();
+const resetDensity = initializeSlider({
+  id: "density",
+  min: 0.01,
+  max: 1000,
+  defaultValueCallback: () => defaultDensity,
+  logarithmic: true,
+  numberOfDecimals: 2,
+  snapPoints: [0.01, 0.02, 0.03, 0.04, 0.05, 1, 100],
+  func: (x) => universe.set_density(x),
+});
+
+let defaultAttractionWeighting = universe.get_attraction_weighting();
+const resetAttractionWeighting = initializeSlider({
+  id: "attraction-weighting",
+  min: 0,
+  max: 1,
+  defaultValueCallback: () => defaultAttractionWeighting,
+  logarithmic: false,
+  numberOfDecimals: 2,
+  func: (x) => universe.set_attraction_weighting(x),
+});
+
+let defaultAttractionRadius = universe.get_attraction_radius();
+const resetAttractionRadius = initializeSlider({
+  id: "attraction-radius",
+  min: 0.01,
+  max: 10,
+  defaultValueCallback: () => defaultAttractionRadius,
+  logarithmic: true,
+  numberOfDecimals: 2,
+  func: (x) => universe.set_attraction_radius(x),
+});
+
+let defaultAlignmentWeighting = universe.get_alignment_weighting();
+const resetAlignmentWeighting = initializeSlider({
+  id: "alignment-weighting",
+  min: 0,
+  max: 1,
+  defaultValueCallback: () => defaultAlignmentWeighting,
+  logarithmic: false,
+  numberOfDecimals: 2,
+  func: (x) => universe.set_alignment_weighting(x),
+});
+
+let defaultAlignmentRadius = universe.get_alignment_radius();
+const resetAlignmentRadius = initializeSlider({
+  id: "alignment-radius",
+  min: 0.01,
+  max: 10,
+  defaultValueCallback: () => defaultAlignmentRadius,
+  logarithmic: true,
+  numberOfDecimals: 2,
+  func: (x) => universe.set_alignment_radius(x),
+});
+
+let defaultSeparationWeighting = universe.get_separation_weighting();
+const resetSeparationWeighting = initializeSlider({
+  id: "separation-weighting",
+  min: 0,
+  max: 1,
+  defaultValueCallback: () => defaultSeparationWeighting,
+  logarithmic: false,
+  numberOfDecimals: 2,
+  func: (x) => universe.set_separation_weighting(x),
+});
+
+let defaultSeparationRadius = universe.get_separation_radius();
+const resetSeparationRadius = initializeSlider({
+  id: "separation-radius",
+  min: 0.01,
+  max: 10,
+  defaultValueCallback: () => defaultSeparationRadius,
+  logarithmic: true,
+  numberOfDecimals: 2,
+  func: (x) => universe.set_seperation_radius(x),
+});
+
+let defaultMaximumVelocity = universe.get_maximum_velocity();
+const resetMaximumVelocity = initializeSlider({
+  id: "maximum-velocity",
+  min: 0.01,
+  max: 1,
+  defaultValueCallback: () => defaultMaximumVelocity,
+  logarithmic: true,
+  numberOfDecimals: 2,
+  func: (x) => universe.set_maximum_velocity(x),
+});
+
+let defaultNoise = universe.get_noise_fraction();
+const resetNoise = initializeSlider({
+  id: "noise",
+  min: 0.01,
+  max: 1,
+  defaultValueCallback: () => defaultNoise,
+  logarithmic: true,
+  numberOfDecimals: 2,
+  func: (x) => universe.set_noise_fraction(x),
+});
+
+function resetAll() {
+  resetNumberOfBoids();
+  resetDensity();
+  resetAttractionWeighting();
+  resetAttractionRadius();
+  resetAlignmentWeighting();
+  resetAlignmentRadius();
+  resetSeparationWeighting();
+  resetSeparationRadius();
+  resetMaximumVelocity();
+  resetNoise();
+}
+
+const preset = document.getElementById("preset");
+preset.value = "basic";
+preset.addEventListener("change", (e) => {
+  switch (e.target.value) {
+    case "basic":
+      universe = wasm.Universe.build_from_preset(wasm.Preset.Basic);
+      break;
+    case "maruyama":
+      universe = wasm.Universe.build_from_preset(wasm.Preset.Maruyama);
+      break;
+    case "zhang":
+      universe = wasm.Universe.build_from_preset(wasm.Preset.Zhang);
+      break;
+    default:
+      throw new Error(`Preset ${e.target.value} not found.`);
+  }
+
+  defaultNumberOfBoids = universe.get_number_of_boids();
+  defaultDensity = universe.get_density();
+  defaultAttractionWeighting = universe.get_attraction_weighting();
+  defaultAttractionRadius = universe.get_attraction_radius();
+  defaultAlignmentWeighting = universe.get_alignment_weighting();
+  defaultAlignmentRadius = universe.get_alignment_radius();
+  defaultSeparationWeighting = universe.get_separation_weighting();
+  defaultSeparationRadius = universe.get_separation_radius();
+  defaultMaximumVelocity = universe.get_maximum_velocity();
+  defaultNoise = universe.get_noise_fraction();
+  resetAll();
 });
 
 play();
