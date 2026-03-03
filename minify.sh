1ead5f798e4f8414c25c80d46959277c745ef638
Kai Chevannes <chevannes.kai@gmail.com>
2026-03-03 05:12:46 +0000

    fix: sed on match start and match end markers

--- minify.sh
+++ minify.sh
@@ -6,15 +6,8 @@ for jsFile in $(find _js -type f); do
 done 
 
 cat _css/*.css > _css/styles.css
-npx esbuild _css/styles.css --bundle --minify --outfile=_css/styles.css
+npx esbuild _css/styles.css --minify --outfile=_css/styles.css
 
 for htmlFile in $(find . -name "index.html"); do
-  perl -0777 -pi -e '
-    s|<!-- MATCH START -->\s*
-      <link rel="stylesheet" href="/_css/base.css" />\s*
-      <link rel="stylesheet" href="/_css/components.css" />\s*
-      <link rel="stylesheet" href="/_css/utilities.css" />\s*
-      <!-- MATCH END -->|
-      <link rel="stylesheet" href="/_css/styles.css" />|gmsx
-  ' "$htmlFile"
+  sed -i -E 's|<!-- MATCH START -->.*<!-- MATCH END -->|<link rel="stylesheet" href="_css/styles.css" />|' "$htmlFile"
 done
