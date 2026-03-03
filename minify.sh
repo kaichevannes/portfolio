483626053fef75ace2aead9d9162da3021dc9cda
Kai Chevannes <chevannes.kai@gmail.com>
2026-03-03 05:04:29 +0000

    fix: use relative url for font source again

--- minify.sh
+++ minify.sh
@@ -6,15 +6,15 @@ for jsFile in $(find _js -type f); do
 done 
 
 cat _css/*.css > _css/styles.css
-npx esbuild _css/styles.css --bundle --minify --outfile=styles.css
+npx esbuild _css/styles.css --bundle --minify --outfile=_css/styles.css
 
-for htmlFile in $(find -name "index.html"); do
+for htmlFile in $(find . -name "index.html"); do
   perl -0777 -pi -e '
-   s|<!-- MATCH START -->\s*
-    <link rel="stylesheet" href="/_css/base.css" />\s*
-    <link rel="stylesheet" href="/_css/components.css" />\s*
-    <link rel="stylesheet" href="/_css/utilities.css" />\s*
-    <!-- MATCH END -->
-    |<link rel="stylesheet" href="styles.css" />|gmx
+    s|<!-- MATCH START -->\s*
+      <link rel="stylesheet" href="/_css/base.css" />\s*
+      <link rel="stylesheet" href="/_css/components.css" />\s*
+      <link rel="stylesheet" href="/_css/utilities.css" />\s*
+      <!-- MATCH END -->|
+      <link rel="stylesheet" href="/_css/styles.css" />|gmsx
   ' "$htmlFile"
 done
