1b155e1e53dd282f97819bc727203597b547f2a5
Kai Chevannes <chevannes.kai@gmail.com>
2026-03-03 05:24:44 +0000

    fix: reference styles from root

--- minify.sh
+++ minify.sh
@@ -9,5 +9,5 @@ cat _css/*.css > _css/styles.css
 npx esbuild _css/styles.css --allow-overwrite --minify --outfile=_css/styles.css
 
 for htmlFile in $(find . -name "index.html"); do
-  perl -0777 -pi -e 's|<!-- MATCH START -->.*<!-- MATCH END -->|<link rel="stylesheet" href="_css/styles.css" />|gs' "$htmlFile"
+  perl -0777 -pi -e 's|<!-- MATCH START -->.*<!-- MATCH END -->|<link rel="stylesheet" href="/_css/styles.css" />|gs' "$htmlFile"
 done
