700a97fe722bba8ecb92f03fdc61aadb440bc35d
Kai Chevannes <chevannes.kai@gmail.com>
2026-03-03 03:27:11 +0000

    perf: generate critical css

---
+++ generate-critical-css.mjs
@@ -0,0 +1,16 @@
+import { execSync } from "child_process";
+import { readdirSync, statSync } from "fs";
+
+const pages = readdirSync(".")
+  .filter((path) => statSync(path).isDirectory())
+  .filter((folder) => !folder.startsWith("_"))
+  .map((folder) => `${folder}/index.html`);
+
+pages.push("index.html");
+
+pages.forEach((page) => {
+  console.log(`Processing ${page}...`);
+  execSync(`npx critical ${page} --inline --base . --target ${page}`, {
+    stdio: "inherit",
+  });
+});
