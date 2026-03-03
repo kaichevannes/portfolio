b37928768842ae40225a0483b71b612581ce1936
Kai Chevannes <chevannes.kai@gmail.com>
2026-03-03 04:45:36 +0000

    feat: minify.sh

---
+++ minify.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+npm install esbuild
+
+for jsFile in $(find _js -type f); do
+  npx esbuild "$jsFile" --minify --outfile="$jsFile"
+done 
+
+cat _css/*.css > _css/styles.css
+npx esbuild _css/styles.css --bundle --minify --outfile=styles.css
+
+for htmlFile in $(find -name "index.html"); do
+  sed -i '' -E 's|\
+<!-- MATCH START -->[[:space:]]*\
+<link rel="stylesheet" href="/_css/base.css" />[[:space:]]*\
+<link rel="stylesheet" href="/_css/components.css" />[[:space:]]*\
+<link rel="stylesheet" href="/_css/utilities.css" />[[:space:]]*\
+<!-- MATCH END -->\
+|<link rel="stylesheet" href="styles.css" />|' \
+  "$htmlFile"
+done
