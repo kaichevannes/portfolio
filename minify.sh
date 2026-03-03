54af66859fd2105558f596afece12653017a19eb
Kai Chevannes <chevannes.kai@gmail.com>
2026-03-03 05:21:06 +0000

    fix: switch to perl for multiline replacement

--- minify.sh
+++ minify.sh
@@ -6,8 +6,8 @@ for jsFile in $(find _js -type f); do
 done 
 
 cat _css/*.css > _css/styles.css
-npx esbuild _css/styles.css --minify --outfile=_css/styles.css
+npx esbuild _css/styles.css --allow-overwrite --minify --outfile=_css/styles.css
 
 for htmlFile in $(find . -name "index.html"); do
-  sed -i -E 's|<!-- MATCH START -->.*<!-- MATCH END -->|<link rel="stylesheet" href="_css/styles.css" />|' "$htmlFile"
+  perl -0777 -pi -e 's|<!-- MATCH START -->.*<!-- MATCH END -->|<link rel="stylesheet" href="_css/styles.css" />|gs' "$htmlFile"
 done
